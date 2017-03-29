9eef2064a751126101cb920863823356ee6467cd
Devin Beliveau <devinmarieb@gmail.com>
2017-03-28 23:00:56 -0600

    Marko strucure, move styles and make component.js

---
+++ components/electron-button/component.js
@@ -0,0 +1,11 @@
+module.exports = {
+  minutes: function() {
+    console.log('Minutes')
+  },
+  hours: function() {
+    console.log('Hours')
+  },
+  days: function() {
+    console.log('Days')
+  }
+}
