31f4f06117711fa722688c7fd6ae3798c987709f
Devin Beliveau <devinmarieb@gmail.com>
2017-03-30 15:54:39 -0600

    Window renders in own screen

--- components/electron-button/component.js
+++ components/electron-button/component.js
@@ -1,18 +1,19 @@
 const electron = require('electron')
 const remote = electron.remote
 const mainProcess = remote.require('./main')
+const currentWindow = remote.getCurrentWindow()
 
 module.exports = {
   read: (e)=> {
     e.preventDefault()
-    window.location.href='https://en.wikipedia.org/wiki/Special:Random'
+    mainProcess.createWindow(window.location.href='https://en.wikipedia.org/wiki/Special:Random')
   },
   watch: (e)=> {
     e.preventDefault()
-    window.location.href='http://random.accessyoutube.org.uk/'
+    mainProcess.createWindow(window.location.href='http://random.accessyoutube.org.uk/')
   },
   listen: (e)=> {
-    window.location.href='https://splice.com/sounds/beatmaker'
+    mainProcess.createWindow(window.location.href='https://splice.com/sounds/beatmaker')
   }
   // draw: (e)=> {
   //   window.location.href='https://quickdraw.withgoogle.com/'
